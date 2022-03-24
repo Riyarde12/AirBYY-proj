a1df14280a81515ddc03431e52a16735d0a373a0
yosefzemertov <yosizemertov@gmail.com>
2022-03-24 15:55:53 +0200

    home page befor css

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -1,51 +1,49 @@
 <template>
   <section class="home-app page-layout">
-    <h1>Air BYY</h1>
-    <div v-if="rooms">
+    <h1>AirBYY</h1>
+    <h2>Travel the world whith AirBYY</h2>
+  <div class="explore-layout" v-if="rooms">
+    <div >
       <div v-for="destination in destinations" :key="destination._id">
         <home-page-preview :destination="destination" @goTo="goTo" />
-        <!-- <div class="container">
-          <div class="coupon" @click="goTo(destination.address.country)">
-            <img src="../assets/img/US.jpg" alt="Avatar" style="width: 100%" />
-            <div class="container" style="background-color: white">
-              <h2><b>20% OFF YOUR PURCHASE</b></h2>
-              <p>
-                Lorem ipsum dolor sit amet, et nam pertinax gloriatur. Sea te
-              </p>
-            </div>
-          </div>
-        </div>
       </div>
-    </div>
-    <hr />
-    <div v-if="rooms">
-      <div v-for="item in topRooms" :key="item._id" class="card-container">
-        <router-link :to="`/room/${item._id}`">
-          <pre>{{ item.name }}</pre>
-        </router-link> -->
+    </div> <br><hr>    
+      <div v-for="room in topRated" :key="room._id">
+          <room-preview :room="room" />    
       </div>
-    </div>
+  </div>
+      
   </section>
 </template>
 
 <script>
 import { utilService } from "../service/util-service.js";
 import homePagePreview from "../components/home-page-preview.vue";
+import roomPreview from "../components/room-preview.vue";
 export default {
   name: "home-app",
   data() {
     return {
       rooms: null,
+      topRated:null
     };
   },
   components: {
     homePagePreview,
+    roomPreview
   },
   methods: {
     goTo(place) {
       // console.log(place);
       this.$router.push({ path: "explore", query: { destination: place } });
     },
+     topRooms() {
+      const topRated = this.rooms.filter(
+        (room) => room.reviewScores.rating > 80
+      );
+      const idx = utilService.getRandomInt(0,topRated.length-4)
+      this.topRated = topRated.slice(idx,idx+4);
+     },
   },
   async created() {
     try {
@@ -64,14 +62,16 @@ export default {
           destinations.push(room);
         }
       });
+      this.topRooms()
       return destinations.slice(0, 4);
     },
-    topRooms() {
-      const topRated = this.rooms.filter(
-        (room) => room.reviewScores.rating > 80
-      );
-      return topRated.slice(0, 4);
-    },
+    // topRooms() {
+    //   const topRated = this.rooms.filter(
+    //     (room) => room.reviewScores.rating > 80
+    //   );
+    //   this.topRated = topRated.slice(0, 4);
+      // return topRated.slice(0, 4);
+    // },
   },
 };
 </script>
