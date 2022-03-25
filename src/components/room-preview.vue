920567d4f45165accb9062b0ec7addb174af67ad
Riyarde12 <yarden.rihsoni03@gmail.com>
2022-03-25 13:00:24 +0300

    setting the router-link  on carousel in preview cmp

--- src/components/room-preview.vue
+++ src/components/room-preview.vue
@@ -1,31 +1,31 @@
 <template>
   <section v-if="room" class="room-preview">
-    <div class="preview-img-section">
-      <el-carousel
-        class="preview-carousel"
-        :autoplay="false"
-        trigger="click"
-        height="200px"
-        arrow="always"
-      >
-        <el-carousel-item v-for="imgUrl in imgForDisplay" :key="imgUrl">
-          <img :src="imgUrl" alt="roomImg" />
-        </el-carousel-item>
-        <svg
-          class="preview-like-btn"
-          viewBox="0 0 32 32"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-hidden="true"
-          role="presentation"
-          focusable="false"
+    <router-link :to="`/room/${room._id}`">
+      <div class="preview-img-section">
+        <el-carousel
+          class="preview-carousel"
+          :autoplay="false"
+          trigger="click"
+          height="200px"
+          arrow="always"
         >
-          <path
-            d="m16 28c7-4.733 14-10 14-17 0-1.792-.683-3.583-2.05-4.95-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05l-2.051 2.051-2.05-2.051c-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05-1.367 1.367-2.051 3.158-2.051 4.95 0 7 7 12.267 14 17z"
-          ></path>
-        </svg>
-      </el-carousel>
-    </div>
-    <router-link :to="'/room/' + room._id">
+          <el-carousel-item v-for="imgUrl in imgForDisplay" :key="imgUrl">
+            <img :src="imgUrl" alt="roomImg" />
+          </el-carousel-item>
+          <svg
+            class="preview-like-btn"
+            viewBox="0 0 32 32"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            role="presentation"
+            focusable="false"
+          >
+            <path
+              d="m16 28c7-4.733 14-10 14-17 0-1.792-.683-3.583-2.05-4.95-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05l-2.051 2.051-2.05-2.051c-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05-1.367 1.367-2.051 3.158-2.051 4.95 0 7 7 12.267 14 17z"
+            ></path>
+          </svg>
+        </el-carousel>
+      </div>
       <div class="preview-details">
         <div class="preview-reviews">
           <div class="preview-rating">
