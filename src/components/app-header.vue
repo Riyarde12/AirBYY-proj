a1fa732ff775b38c74acc078cd3cc4b458f17593
Ben <Benl000100@gmail.com>
2022-03-26 05:29:50 +0300

    search btn

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -12,7 +12,29 @@
         </router-link>
       </section>
       <section class="search">
-        <div class="tsearch">search</div>
+        <button class="search-btn">
+          <h4>Start your search</h4>
+          <div
+            class="el-button el-button--danger el-button--large is-circle"
+            type="button"
+          >
+            <i class="el-icon">
+              <svg
+                viewBox="0 0 32 32"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                role="presentation"
+                focusable="false"
+              >
+                <g fill="none">
+                  <path
+                    d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"
+                  ></path>
+                </g>
+              </svg>
+            </i>
+          </div>
+        </button>
       </section>
       <!-- <search-bar /> -->
       <section class="user-btn">
