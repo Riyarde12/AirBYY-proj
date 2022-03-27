fc5126a0e623ca3e1c072e112a9dfdca17277641
Riyarde12 <yarden.rihsoni03@gmail.com>
2022-03-27 17:45:21 +0300

    test

--- src/components/home-page-preview.vue
+++ src/components/home-page-preview.vue
@@ -1,28 +1,34 @@
 <template>
-  <section
-    class="home-page-preview"
-    @click="country(destination.address.country)"
-  >
-    <img class="preview-carousel" src="src\assets\img\030.jpeg" alt="" />
-    <div class="location-title">
-      <h1>{{ destination.address.country }}</h1>
-      <h6>46 kilometers away</h6>
-    </div>
-  </section>
+	<section
+		class="home-page-preview"
+		@click="country(destination.address.country)"
+	>
+		<img class="preview-carousel" src="getImg" alt="" />
+		<div class="location-title">
+			<h1>{{ destination.address.country }}</h1>
+			<h6>46 kilometers away</h6>
+		</div>
+	</section>
 </template>
 
 <script>
-export default {
-  props: {
-    destination: Object,
-  },
-  methods: {
-    country(place) {
-      // console.log(place);
-      this.$emit("goTo", place);
-    },
-  },
-};
+	export default {
+		props: {
+			destination: Object,
+		},
+		methods: {
+			country(place) {
+				// console.log(place);
+				this.$emit("goTo", place);
+			},
+		},
+		computed: {
+			getImg() {
+				// return `@/assets/img/${this.destination.address.country}.jpg`;
+				return `frontend/src/assets/img/Brazil.jpg`;
+			},
+		},
+	};
 </script>
 
 <style></style>
