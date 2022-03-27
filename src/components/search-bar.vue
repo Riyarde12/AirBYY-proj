6a0b3f0fd6065ce95be1526c22f3c5161a8f60c8
yosefzemertov <yosizemertov@gmail.com>
2022-03-27 14:18:43 +0200

    after change

--- src/components/search-bar.vue
+++ src/components/search-bar.vue
@@ -1,35 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <section class="search-section">
-    <div class="search-bar">
-      <div class="flex">
-        <!-- <div @click="modalDestination = !modalDestination" class="filter-tag 1 fd"><p class="bold">Destination</p> -->
-        <div
-          @click="clickedModal('modalDestination')"
-          :class="modalDestination ? 'choose' : ''"
-          class="filter-tag 1 fd"
-        >
-          <p class="bold title-tag">Location</p>
-          <input
-          @input="destinationToDisplay"
-            type="text"
-            v-model="filterBy.destination"
-            placeholder="Where are you going?"
-          />
-        </div>
-        <!-- <p>Where are you going?</p></div> -->
-        <div class="vert"></div>
-        <div
-          @click="clickedModal('modalDate', 4)"
-          :class="modal4 ? 'choose' : ''"
-          class="filter-tag first-date"
-        >
-          <p class="bold title-tag">Check in</p>
-          <p>Add dates</p>
-        </div>
-        <div class="vert"></div>
-        <!-- DATE-PICKER -->
-=======
 	<section class="search-section">
 		<div class="search-bar">
 			<div class="flex">
@@ -58,7 +27,6 @@
 				</div>
 				<div class="vert"></div>
 				<!-- DATE-PICKER -->
->>>>>>> 3042676e4947c2a0adb08bfe403b770f6d5f1379
 
 				<!-- DATE-PICKER -->
 				<div
@@ -131,40 +99,6 @@
 					</div>
 				</div>
 
-<<<<<<< HEAD
-        <div class="line flex">
-          <div class="txt">
-            <h3 class="title-tag">Pets</h3>
-            <p class="content under-line">Bringing a service animal?</p>
-          </div>
-          <div class="flex main-line">
-            <div @click="add(0, 'pets')" class="div-btn">-</div>
-            <p class="number">{{ filterBy.pets }}</p>
-            <div @click="add(1, 'pets')" class="div-btn">+</div>
-          </div>
-        </div>
-        <p class="bottomModal">
-          If you're lucky enough to have more than 2 pets with you, be sure to
-          let your host know.
-        </p>
-      </div>
-    </teleport>
-    <teleport to="#models">
-      <div v-if="modalDate" class="modal-2">
-        <h1>date</h1>
-      </div>
-    </teleport>
-    <teleport to="#models">
-      <div v-if="modalDestination" class="modal-3">
-        <ul>
-          <li>d</li>
-          <li>d</li>
-          <li>d</li>
-          <li>d</li>
-        </ul>
-      </div>
-    </teleport>
-=======
 				<div class="line flex">
 					<div class="txt">
 						<h3 class="title-tag">Pets</h3>
@@ -192,7 +126,6 @@
 				<h1>date</h1>
 			</div>
 		</teleport>
->>>>>>> 3042676e4947c2a0adb08bfe403b770f6d5f1379
 
 		<div class="demo-date-picker">
 			<div class="block">
@@ -210,117 +143,6 @@
 </template>
 
 <script>
-<<<<<<< HEAD
-import { Search } from "@element-plus/icons-vue";
-import { shallowRef } from "@vue/reactivity";
-// import { el } from "element-plus/lib/locale";
-const searchIcon = shallowRef(Search);
-export default {
-  data() {
-    return {
-      filterBy: {
-        adults: 0,
-        children: 0,
-        infants: 0,
-        pets: 0,
-        destination: "",
-      },
-      modalGuests: false,
-      modalDate: false,
-      modalDestination: false,
-      modal4: false,
-      modal5: false,
-      searchIcon,
-      searchBarTaped: true,
-    };
-  },
-  created() {
-    const params = this.$route.query;
-    if (params.destination) {
-      this.filterBy.destination = params.destination;
-    }
-  },
-  methods: {
-    add(addedVal, key) {
-      if (addedVal) {
-        this.filterBy[key]++;
-      } else {
-        if (this.filterBy[key] <= 0) return;
-        this.filterBy[key]--;
-      }
-      console.log("this.filterBy.adults", this.filterBy.adults);
-    },
-    sendFilter() {
-      this.$router.push({
-        path: "explore",
-        query: { destination: this.filterBy.destination },
-      });
-      this.$store.dispatch({
-        type: "loadRooms",
-        filterBy: { ...this.filterBy },
-      });
-    },
-    clickedModal(modal, modal45 = null) {
-      if (modal === "modalDate") {
-        if (modal45 === 4) {
-          if (this.modal4 === true) {
-            this.modal4 = false;
-            this.modalDate = false;
-          } else {
-            this.modal4 = true;
-            this.modal5 = false;
-            this.modalDate = true;
-            console.log("modal4", this.modal4);
-            console.log("modal5", this.modal5);
-            this.$refs.input.focus();
-          }
-        } else {
-          if (this.modal5 === true) {
-            this.modal5 = false;
-            this.modalDate = false;
-          } else {
-            this.$refs.input.focus();
-            this.modal5 = true;
-            this.modal4 = false;
-            this.modalDate = true;
-          }
-        }
-        this.modalGuests = false;
-        this.modalDestination = false;
-        return;
-      } else {
-        this.modal4 = false;
-        this.modal5 = false;
-      }
-      if (this[modal] === true) {
-        this[modal] = false;
-        return;
-      }
-      this.modalGuests = false;
-      this.modalDate = false;
-      this.modalDestination = false;
-      this[modal] = true;
-    },
-    // destinationToDisplay(){
-    //    try {
-    //   await this.$store.dispatch({
-    //     type: "loadRooms",
-    //     filterBy: params,
-    //   });
-    //   this.rooms = this.$store.getters.rooms;
-    //   console.log("this.rooms", this.rooms);
-    // } catch (err) {
-    //   console.log("err", err);
-    // }
-    // }
-  },
-  computed: {
-    adults() {
-      return this.filterBy.children;
-    },
-  },
-};
-=======
 	import { Search } from "@element-plus/icons-vue";
 	import { shallowRef } from "@vue/reactivity";
 	// import { el } from "element-plus/lib/locale";
@@ -421,7 +243,6 @@ export default {
 			},
 		},
 	};
->>>>>>> 3042676e4947c2a0adb08bfe403b770f6d5f1379
 </script>
 <style>
 	.vert {
