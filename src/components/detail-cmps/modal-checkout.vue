987121e787b27294fafa30e555f19912d9cf3648
Ben <Benl000100@gmail.com>
2022-03-28 21:14:37 +0300

    room app details

--- src/components/detail-cmps/modal-checkout.vue
+++ src/components/detail-cmps/modal-checkout.vue
@@ -1,55 +1,54 @@
 <template>
-	<section class="modal-display-checkout sticky">
-		<div class="checkout-modal">
-			<div class="checkout-container flex">
-				<div>
-					<div class="tools-container flex space-between align-center">
-						<div class="top-modal-container flex">
-							<div v-if="room">${{ showPrice }} / Night</div>
-							<div class="display-review-info flex">
-								<span><img src="../../assets/img/star.png" alt="" /></span>
-								<span v-if="room">{{ avgReviewScores }} </span>
-								<span class="under-line">{{ room.numOfReviews }} reviews</span>
-							</div>
-						</div>
+  <section class="modal-display-checkout sticky">
+    <div class="checkout-modal">
+      <div class="checkout-container flex">
+        <div class="tools-container flex space-between align-center">
+          <div class="top-modal-container flex">
+            <div v-if="room">${{ showPrice }} / Night</div>
+            <div class="display-review-info flex">
+              <span><img src="../../assets/img/star.png" alt="" /></span>
+              <span v-if="room">{{ avgReviewScores }} </span>
+              <span class="under-line">{{ room.numOfReviews }} reviews</span>
+            </div>
+          </div>
 
-						<div class="display-order-container pointer">
-							<div class="checkin-checkout-container flex">
-								<div class="checkin-btn">
-									<div class="flex">
-										<div class="add-dates-container flex">
-											<div>
-												<div class="checkin-out">CHECK-IN</div>
-												<div class="add-date">Add date</div>
+          <div class="display-order-container pointer">
+            <div class="checkin-checkout-container flex">
+              <div class="checkin-btn">
+                <div class="flex">
+                  <div class="add-dates-container flex">
+                    <div>
+                      <div class="checkin-out">CHECK-IN</div>
+                      <div class="add-date">Add date</div>
 
-												<!-- DATE-PICKER -->
-												<div class="datePicker">
-													<div class="demo-date-picker">
-														<div class="block">
-															<el-date-picker
-																type="daterange"
-																range-separator="To"
-																start-placeholder="CHECK-IN"
-																end-placeholder="CHECKOUT"
-															/>
-														</div>
-													</div>
-												</div>
-												<!-- DATE-PICKER -->
-											</div>
-										</div>
-										<div class="add-dates-container flex border-left">
-											<div class="checkin-out">CHECKOUT</div>
-											<div class="add-date">Add dates</div>
-										</div>
-									</div>
-									<div
-										class="add-guests-container flex space-between"
-										@click.stop="addGuests"
-									>
-										<div class="guests">GUESTS</div>
-										<div>
-											<!-- <svg
+                      <!-- DATE-PICKER -->
+                      <div class="datePicker">
+                        <div class="demo-date-picker">
+                          <div class="block">
+                            <el-date-picker
+                              type="daterange"
+                              range-separator="To"
+                              start-placeholder="CHECK-IN"
+                              end-placeholder="CHECKOUT"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <!-- DATE-PICKER -->
+                    </div>
+                  </div>
+                  <div class="add-dates-container flex border-left">
+                    <div class="checkin-out">CHECKOUT</div>
+                    <div class="add-date">Add dates</div>
+                  </div>
+                </div>
+                <div
+                  class="add-guests-container flex space-between"
+                  @click.stop="addGuests"
+                >
+                  <div class="guests">GUESTS</div>
+                  <div>
+                    <!-- <svg
 												viewBox="0 0 32 32"
 												xmlns="http://www.w3.org/2000/svg"
 												aria-hidden="true"
@@ -62,87 +61,86 @@
 													></path>
 												</g>
 											</svg> -->
-										</div>
-										<guests-modal
-											v-if="openGuestsModal"
-											:currOrder="currOrder"
-											@onCloseModal="closeModal"
-											@onRemove="onRemove"
-											@onAdd="onAdd"
-										/>
-									</div>
-								</div>
-							</div>
-							<div class="availability-container" @click="onReserve">
-								Check availability
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+                  </div>
+                  <guests-modal
+                    v-if="openGuestsModal"
+                    :currOrder="currOrder"
+                    @onCloseModal="closeModal"
+                    @onRemove="onRemove"
+                    @onAdd="onAdd"
+                  />
+                </div>
+              </div>
+            </div>
+            <button class="availability-container" @click="onReserve">
+              Check availability
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 <script>
-	import guestsModal from "./guests-modal.vue";
-	export default {
-		name: "modal-checkout",
-		components: {
-			guestsModal,
-		},
-		props: {
-			room: {
-				type: Object,
-				require: true,
-			},
-			preOrder: { type: Object },
-		},
-		data() {
-			return {
-				openGuestsModal: false,
-				currOrder: this.preOrder,
-			};
-		},
-		created() {},
-		methods: {
-			updatePreOrder() {},
-			addGuests() {
-				console.log("hey");
-				this.openGuestsModal = !this.openGuestsModal;
-				console.log("this.openGuestsModal", this.openGuestsModal);
-			},
-			closeModal(guests) {
-				this.openGuestsModal = false;
-				console.log("this.openGuestsModal", this.openGuestsModal);
-				this.currOrder.guests = guests;
-			},
-			onRemove(guest) {
-				if (this.currOrder.guests[guest] <= 0) return;
-				this.currOrder.guests[guest]--;
-				console.log(this.currOrder);
-			},
-			onAdd(guest) {
-				this.currOrder.guests[guest]++;
-				console.log(this.currOrder);
-			},
-			onReserve() {
-				console.log("this.currOrder", this.currOrder);
-				this.$emit("onReserve", { ...this.currOrder });
-			},
-		},
-		computed: {
-			showPrice() {
-				return this.room.price;
-			},
-			avgReviewScores() {
-				return this.room.reviewScores.rating / 20;
-			},
-		},
-	};
+import guestsModal from "./guests-modal.vue";
+export default {
+  name: "modal-checkout",
+  components: {
+    guestsModal,
+  },
+  props: {
+    room: {
+      type: Object,
+      require: true,
+    },
+    preOrder: { type: Object },
+  },
+  data() {
+    return {
+      openGuestsModal: false,
+      currOrder: this.preOrder,
+    };
+  },
+  created() {},
+  methods: {
+    updatePreOrder() {},
+    addGuests() {
+      console.log("hey");
+      this.openGuestsModal = !this.openGuestsModal;
+      console.log("this.openGuestsModal", this.openGuestsModal);
+    },
+    closeModal(guests) {
+      this.openGuestsModal = false;
+      console.log("this.openGuestsModal", this.openGuestsModal);
+      this.currOrder.guests = guests;
+    },
+    onRemove(guest) {
+      if (this.currOrder.guests[guest] <= 0) return;
+      this.currOrder.guests[guest]--;
+      console.log(this.currOrder);
+    },
+    onAdd(guest) {
+      this.currOrder.guests[guest]++;
+      console.log(this.currOrder);
+    },
+    onReserve() {
+      console.log("this.currOrder", this.currOrder);
+      this.$emit("onReserve", { ...this.currOrder });
+    },
+  },
+  computed: {
+    showPrice() {
+      return this.room.price;
+    },
+    avgReviewScores() {
+      return this.room.reviewScores.rating / 20;
+    },
+  },
+};
 </script>
 
 <style>
-.el-date-editor{
+.el-date-editor {
   left: 205px;
   bottom: 60px;
   /* z-index: -1; */
@@ -152,9 +150,8 @@
   height: 1px !important;
   /* --el-input-hover-border-color:none !important */
 }
-.el-picker-panel{
+.el-picker-panel {
   z-index: 100 !important;
   /* position: relative !important; */
 }
-
 </style>
