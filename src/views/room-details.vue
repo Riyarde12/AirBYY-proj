af605f25c68a7a5fc821ceda24e562138fe3c55a
Ben <Benl000100@gmail.com>
2022-03-30 04:57:51 +0300

    scss

--- src/views/room-details.vue
+++ src/views/room-details.vue
@@ -15,6 +15,7 @@
           <h4 class="under-line">{{ room.address.street }}</h4>
         </div>
       </div>
+
       <div class="options-btn flex">
         <button class="flex">
           <svg
@@ -39,7 +40,7 @@
               <path d="M6 13l9.293-9.293a1 1 0 0 1 1.414 0L26 13"></path>
             </g>
           </svg>
-          <p class="under-line">Save</p>
+          <p class="under-line">Share</p>
         </button>
         <button class="flex">
           <svg
@@ -62,7 +63,7 @@
               d="m16 28c7-4.733 14-10 14-17 0-1.792-.683-3.583-2.05-4.95-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05l-2.051 2.051-2.05-2.051c-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05-1.367 1.367-2.051 3.158-2.051 4.95 0 7 7 12.267 14 17z"
             ></path>
           </svg>
-          <p class="under-line">Share</p>
+          <p class="under-line">Save</p>
         </button>
       </div>
     </section>
