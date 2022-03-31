08a1bdb1126a3ce0c9d1cdcacdc450e889d0d150
Riyarde12 <yarden.rihsoni03@gmail.com>
2022-03-31 05:39:40 +0300

    fix login modal

--- src/components/user-cmps/login-modal.vue
+++ src/components/user-cmps/login-modal.vue
@@ -1,92 +1,92 @@
 <template>
-  <section class="login-modal">
-    <Transition name="modal">
-      <div v-if="show" class="modal-mask">
-        <div class="modal-wrapper">
-          <div class="modal-container">
-            <h4>log in or sign up</h4>
-            <h1>Welcome to Airbyy</h1>
-            <button @click="onToggle">log in/sign up</button>
-            <hr />
-            <form v-if="!signUp">
-              <h1>sign in</h1>
-              <input type="text" placeholder="username" v-model="username" />
-              <input type="text" placeholder="password" v-model="password" />
-              <button @click="onLogin">Continue</button>
-              <button class="modal-default-button" @click.prevent="signup">
-                OK
-              </button>
-              <hr />
-            </form>
-            <form v-if="signUp">
-              <h1>sign up</h1>
-              <input type="text" placeholder="Fullname" v-model="fullname" />
-              <input type="text" placeholder="Username" v-model="username" />
-              <input type="text" placeholder="password" v-model="password" />
-              <hr />
-              <p>
-                We’ll call or text you to confirm your number. Standard message
-                and data rates apply. <span>Privacy Policy</span>
-              </p>
-              <button class="modal-default-button" @click.prevent="signup">
-                Continue
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </Transition>
-  </section>
+	<section class="login-modal">
+		<Transition name="modal">
+			<div v-if="show" class="modal-mask">
+				<div class="modal-wrapper">
+					<div class="modal-container">
+						<h4>log in or sign up</h4>
+						<h1>Welcome to Airbyy</h1>
+						<button @click="onToggle">log in/sign up</button>
+						<hr />
+						<form v-if="!signUp">
+							<h1>sign in</h1>
+							<input type="text" placeholder="username" v-model="username" />
+							<input type="text" placeholder="password" v-model="password" />
+							<button class="modal-default-button" @click.prevent="onLogin">
+								Continue
+							</button>
+
+							<hr />
+						</form>
+						<form v-if="signUp">
+							<h1>sign up</h1>
+							<input type="text" placeholder="Fullname" v-model="fullname" />
+							<input type="text" placeholder="Username" v-model="username" />
+							<input type="text" placeholder="password" v-model="password" />
+							<hr />
+							<p>
+								We’ll call or text you to confirm your number. Standard message
+								and data rates apply. <span>Privacy Policy</span>
+							</p>
+							<button class="modal-default-button" @click.prevent="signup">
+								Continue
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</Transition>
+	</section>
 </template>
 
 <script>
-export default {
-  name: "login-modal",
-  props: {
-    show: Boolean,
-  },
-  data() {
-    return {
-      fullname: "",
-      username: "",
-      password: "",
-      loggedInUser: null,
-      signUp: false,
-    };
-  },
-  methods: {
-    async onLogin() {
-      try {
-        const user = await this.$store.dispatch({
-          type: "login",
-          username: this.username,
-          password: this.password,
-        });
-        this.loggedInUser = user.data;
-        this.$emit("close");
-        // console.log("example", this.loggedInUser);
-        // console.log("username:", this.username);
-        // console.log("password:", this.password);
-      } catch (err) {
-        console.log("Cannot login", err);
-      }
-    },
-    signup() {
-      this.$store.dispatch({
-        type: "signUp",
-        userSignUp: {
-          fullname: this.fullname,
-          username: this.username,
-          password: this.password,
-        },
-      });
-    },
-    onToggle() {
-      this.signUp = !this.signUp;
-    },
-  },
-  computed: {},
-};
+	export default {
+		name: "login-modal",
+		props: {
+			show: Boolean,
+		},
+		data() {
+			return {
+				fullname: "",
+				username: "",
+				password: "",
+				loggedInUser: null,
+				signUp: false,
+			};
+		},
+		methods: {
+			async onLogin() {
+				try {
+					const user = await this.$store.dispatch({
+						type: "login",
+						username: this.username,
+						password: this.password,
+					});
+					this.loggedInUser = user.data;
+					this.$emit("close");
+					// console.log("example", this.loggedInUser);
+					// console.log("username:", this.username);
+					// console.log("password:", this.password);
+				} catch (err) {
+					console.log("Cannot login", err);
+				}
+			},
+			signup() {
+				this.$store.dispatch({
+					type: "signUp",
+					userSignUp: {
+						fullname: this.fullname,
+						username: this.username,
+						password: this.password,
+					},
+				});
+			},
+			onToggle() {
+				this.signUp = !this.signUp;
+			},
+		},
+		computed: {},
+	};
 </script>
 
 <style>
