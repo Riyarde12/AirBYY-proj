55e401be21d10bcb5569b5831f444524b5f5219f
Ben <Benl000100@gmail.com>
2022-03-31 04:39:12 +0300

    MODAL LOG IN

--- src/components/header-cmps/user-modal-log.vue
+++ src/components/header-cmps/user-modal-log.vue
@@ -1,27 +1,27 @@
 <template>
-	<section class="user-login-modal">
-		<button @click="openModalLogin">Log in</button>
-		<button>sign up</button>
-		<div class="break"></div>
-		<button>Host your home</button>
-		<button>Host an experience</button>
-		<button>Help</button>
-	</section>
+  <section class="user-login-modal">
+    <button @click="openModalLogin">Log in</button>
+    <button>Sign up</button>
+    <div class="break"></div>
+    <button>Host your home</button>
+    <button>Host an experience</button>
+    <button>Help</button>
+  </section>
 </template>
 
 <script>
-	export default {
-		components: {},
-		data() {
-			return {};
-		},
-		methods: {
-			openModalLogin() {
-				console.log("hey");
-				this.$emit("openModalLogin");
-			},
-		},
-	};
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {
+    openModalLogin() {
+      console.log("hey");
+      this.$emit("openModalLogin");
+    },
+  },
+};
 </script>
 
 <style>
