3380c870bce6db3814ec02c0707aede36e92a767
Ben <Benl000100@gmail.com>
2022-03-31 08:44:42 +0300

    log in modal,ask-login modal-scss

--- src/components/user-cmps/ask-login.vue
+++ src/components/user-cmps/ask-login.vue
@@ -1,129 +1,78 @@
 <template>
-	<section>
-		<!-- ASK_GUEST -->
+  <section class="ask-login">
+    <!-- ASK_GUEST -->
 
-		<Transition name="modal">
-			<div v-if="show" class="modal-mask">
-				<div class="modal-wrapper">
-					<div class="modal-container">
-						<div class="modal-header">
-							<slot name="header"></slot>
-						</div>
-						<div class="modal-body">
-							<button id="show-modal" @click="onLogin">Log in</button>
-						</div>
-						<div class="modal-footer">
-							<slot name="footer">
-								Stay guest :
-								<button class="modal-default-button" @click="$emit('close')">
-									OK
-								</button>
-							</slot>
-						</div>
-					</div>
-				</div>
-			</div>
-		</Transition>
+    <Transition name="modal">
+      <div v-if="show" class="modal-mask">
+        <div class="modal-wrapper">
+          <div class="modal-container">
+            <svg
+              viewBox="0 0 32 32"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              role="presentation"
+              focusable="false"
+            >
+              <path d="m6 6 20 20"></path>
+              <path d="m26 6-20 20"></path>
+            </svg>
+            <div class="modal-header">
+              <h1 name="header">You are not logged in</h1>
+            </div>
+            <div class="modal-body">
+              <button class="tracking" id="show-modal" @click="onLogin">
+                <span> Log in</span>
+              </button>
+            </div>
+            <div class="modal-footer">
+              <div name="footer">
+                <button class="modal-default-button" @click="$emit('close')">
+                  Stay guest
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Transition>
 
-		<!-- LOGIN-SIGNUP -->
+    <!-- LOGIN-SIGNUP -->
 
-		<Teleport to="body">
-			<login-modal :show="loginModalOpen" @close="showModal">
-				<template #header>
-					<h3>custom header</h3>
-				</template>
-			</login-modal>
-		</Teleport>
-	</section>
+    <Teleport to="body">
+      <login-modal :show="loginModalOpen" @close="showModal">
+        <template #header>
+          <h3>custom header</h3>
+        </template>
+      </login-modal>
+    </Teleport>
+  </section>
 </template>
 
 
 <script>
-	import loginModal from "./login-modal.vue";
-	export default {
-		name: "ask-login",
-		props: {
-			show: Boolean,
-		},
-		data() {
-			return {
-				loginModalOpen: false,
-			};
-		},
-		components: {
-			loginModal,
-		},
-		methods: {
-			onLogin() {
-				this.loginModalOpen = true;
-				this.show = false;
-			},
-		},
-	};
+import loginModal from "./login-modal.vue";
+export default {
+  name: "ask-login",
+  props: {
+    show: Boolean,
+  },
+  data() {
+    return {
+      loginModalOpen: false,
+    };
+  },
+  components: {
+    loginModal,
+  },
+  methods: {
+    onLogin() {
+      this.loginModalOpen = true;
+      this.show = false;
+    },
+  },
+};
 </script>
 
 
 <style>
-.modal-mask {
-  position: fixed;
-  z-index: 9998;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: table;
-  transition: opacity 0.3s ease;
-}
-
-.modal-wrapper {
-  display: table-cell;
-  vertical-align: middle;
-}
-
-.modal-container {
-  width: 300px;
-  margin: 0px auto;
-  padding: 20px 30px;
-  background-color: #fff;
-  border-radius: 2px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
-  transition: all 0.3s ease;
-}
-
-.modal-header h3 {
-  margin-top: 0;
-  color: #42b983;
-}
-
-.modal-body {
-  margin: 20px 0;
-}
-
-.modal-default-button {
-  float: right;
-}
-
-/*
- * The following styles are auto-applied to elements with
- * transition="modal" when their visibility is toggled
- * by Vue.js.
- *
- * You can easily play with the modal transition by editing
- * these styles.
- */
-
-.modal-enter-from {
-  opacity: 0;
-}
-
-.modal-leave-to {
-  opacity: 0;
-}
-
-.modal-enter-from .modal-container,
-.modal-leave-to .modal-container {
-  -webkit-transform: scale(1.1);
-  transform: scale(1.1);
-}
 </style>
