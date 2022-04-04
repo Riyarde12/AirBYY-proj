52ab81ab7f0dd24caf23a58dc8ea651ef034fb0b
Riyarde12 <yarden.rihsoni03@gmail.com>
2022-04-04 18:59:25 +0300

    setting imgs for heroku

--- src/components/home-page-preview.vue
+++ src/components/home-page-preview.vue
@@ -1,43 +1,43 @@
 <template>
-  <section class="home-page-preview" @click="country(destination.address.city)">
-    <img class="preview-carousel" :src="getImg" alt="" />
-    <div class="location-title">
-      <h1>{{ destination.address.city }}</h1>
-      <h6>{{ getKm }} kilometers away</h6>
-    </div>
-  </section>
+	<section class="home-page-preview" @click="country(destination.address.city)">
+		<img class="preview-carousel" :src="getImg" alt="" />
+		<div class="location-title">
+			<h1>{{ destination.address.city }}</h1>
+			<h6>{{ getKm }} kilometers away</h6>
+		</div>
+	</section>
 </template>
 
 <script>
-export default {
-  props: {
-    destination: Object,
-  },
-  methods: {
-    country(place) {
-      // console.log(place);
-      this.$emit("goTo", place);
-    },
-  },
-  computed: {
-    getImg() {
-      return `src/assets/img/${this.destination.address.city}.jpg`;
-    },
-    getKm() {
-      const city = this.destination.address.city;
-      switch (city) {
-        case "Montreal":
-          return "9,537";
-        case "Porto":
-          return "3,970";
-        case "New York":
-          return "10,853";
-        case "Barcelona":
-          return "3,584";
-      }
-    },
-  },
-};
+	export default {
+		props: {
+			destination: Object,
+		},
+		methods: {
+			country(place) {
+				// console.log(place);
+				this.$emit("goTo", place);
+			},
+		},
+		computed: {
+			getImg() {
+				return `https://res.cloudinary.com/canjan22/image/upload/v1649081637/AirByy/${this.destination.address.city}.jpg`;
+			},
+			getKm() {
+				const city = this.destination.address.city;
+				switch (city) {
+					case "Montreal":
+						return "9,537";
+					case "Porto":
+						return "3,970";
+					case "New York":
+						return "10,853";
+					case "Barcelona":
+						return "3,584";
+				}
+			},
+		},
+	};
 </script>
 
 <style></style>
