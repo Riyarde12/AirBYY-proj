cf0d61f0f13af1976b1535619354f7f274bba853
yosefzemertov <yosizemertov@gmail.com>
2022-04-05 04:16:07 +0300

    signout

--- src/service/socket-service.js
+++ src/service/socket-service.js
@@ -6,7 +6,8 @@ export const SOCKET_EVENT_REVIEW_ADDED = 'review-added';
 export const SOCKET_EVENT_REVIEW_ABOUT_YOU = 'review-about-you';
 
 
-const baseUrl = (process.env.NODE_ENV === 'production')? '' : '//localhost:3030'
+// const baseUrl = (process.env.NODE_ENV === 'production')? '' : '//localhost:3030'
+const baseUrl =  '//localhost:3030'
 export const socketService = createSocketService()
 // export const socketService = createDummySocketService()
 
